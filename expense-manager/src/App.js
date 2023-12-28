7ee5c95401db333ee1e70d749e4e2483fd9ad834
Lakruwan Jayathissa <lakruwan987@gmail.com>
2023-12-29 02:39:35 +0530

    creating project

---
+++ expense-manager/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+import MainDashboard from './MainDashboard';
+
+function App() {
+  return (
+    <div>
+   
+  <h1> 
+
+  <MainDashboard/>
+
+  </h1>
+    
+    
+    
+ 
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ expense-manager/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      'yellow': '#ffc82c',
+      'blue': '#200E3A',
+      'blue1': '#38419D',
+      'blue2': '#3887BE',
+      'blue3': '#52D3D8'
+      },
+    },
+  },
+  plugins: [],
+  
+}
+
